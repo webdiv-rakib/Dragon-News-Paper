cc256dfff07dfd8cc36f85bf17f24376765747c6
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-05-14 01:34:25 +0600

    sticky class added where needed.

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -19,13 +19,13 @@ const HomeLayout = () => {
                 </nav>
             </header>
             <main className='w-11/12 mx-auto pt-10 grid grid-cols-12 gap-5'>
-                <aside className='col-span-3'>
+                <aside className='col-span-3 sticky h-fit top-0'>
                     <LeftAside></LeftAside>
                 </aside>
                 <section className='main-nav col-span-6'>
                     <Outlet></Outlet>
                 </section>
-                <aside className='col-span-3'>
+                <aside className='col-span-3 sticky h-fit top-0'>
                     <RightAside></RightAside>
                 </aside>
             </main>
