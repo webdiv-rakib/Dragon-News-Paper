4320ce636969b3c47b4593226fe355e6105959a3
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-05-13 17:42:39 +0600

    Load News Data & Filter News Data based on Category

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
+    // useParams for get button id
     const { id } = useParams();
-    console.log(id)
+
+    // useLoaderData for loading data
+    const data = useLoaderData();
+
+    // id wise news render data to show on UI
+    const [categoryNews, setCategoryNews] = useState([]);
+
+    // useEffect to filter id wise news
+    useEffect(() => {
+        // now we use if else if else condition to render conditional data.
+        if (id == "0") {
+            setCategoryNews(data);
+            return;
+        }
+        else if (id == '1') {
+            const filteredNews = data.filter(news => news.others.is_today_pick == true);
+            setCategoryNews(filteredNews)
+        }
+        else {
+
+            const filteredNews = data.filter(news => news.category_id == id);
+            setCategoryNews(filteredNews)
+        }
+    }, [data, id]);
     return (
         <div>
-            <h1 className='font-semibold'>Category News: {id}</h1>
+            <h1 className='font-semibold'>Total: {categoryNews.length} News Found</h1>
         </div>
     );
 };
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -15,7 +15,8 @@ const router = createBrowserRouter(
                 },
                 {
                     path: '/category/:id',
-                    element: <CategoryNews></CategoryNews>
+                    element: <CategoryNews></CategoryNews>,
+                    loader: () => fetch('/news.json')
                 }
             ]
         },
