044274a9ae34509d190e51ea95c762a6f3daf0d2
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-05-14 00:08:00 +0600

    Qzone design complete.

--- src/components/homelayout/Qzone.jsx
+++ src/components/homelayout/Qzone.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import swimming from '../../assets/swimming.png'
+import classroom from '../../assets/class.png'
+import playground from '../../assets/playground.png'
 
 const Qzone = () => {
     return (
-        <div className='pt-5'>
+        <div className='pt-5 bg-base-200 p-5 mt-5'>
             <div>
                 <h1 className='font-semibold'>Q Zone</h1>
+            </div>
+            <div>
+                <img src={swimming} alt="" />
+                <img src={classroom} alt="" />
+                <img src={playground} alt="" />
             </div>
         </div>
     );
