64f351c927dfaefb232fa911f8046f40035bd59f
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-05-14 01:10:45 +0600

    explore Navigate component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
     return (
-        <div>
-            <h1 className='font-semibold'>Dragon News Home</h1>
-        </div>
+        <Navigate to='/category/1'></Navigate>
     );
 };
 
